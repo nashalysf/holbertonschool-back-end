5cafa7b06eef6747bd98cb592eb59b8461ef901a
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-21 15:48:28 -0400

    exports todo info in json format

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,2 +1,30 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""
+RESTFul API for employee
+"""
+import requests
+import sys
+import json
+
 if __name__ == "__main__":
+    """ Gets employee info in json format """
+    id = sys.argv[1]
+    tasks = []
+
+
+user_url = requests.get(
+    'https://jsonplaceholder.typicode.com/' + 'users/' + id).json()
+todos = requests.get(
+    'https://jsonplaceholder.typicode.com/' + 'todos?userId=' + id).json()
+
+for task in todos:
+    task_data = {
+        'task': task['title'],
+        'completed': task['completed'],
+        'username': user_url['username']
+    }
+    tasks.append(task_data)
+data = {id: tasks}
+
+with open(id + '.json', 'w') as f:
+    json.dump(data, f)
