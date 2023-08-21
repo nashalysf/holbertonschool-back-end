c03f1eec355f927cda3e18200b5dcffd29e39370
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-21 14:16:56 -0400

    fixing typos

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,6 +2,8 @@
 """
 RESTFul API for employee
 """
+
+
 import requests
 import sys
 
@@ -9,22 +11,23 @@ import sys
 if __name__ == "__main__":
     """ Gets employee todo information """
     id = sys.argv[1]
-    task_title = []
-    completed = 0
-    total = 0
+    TASK_TITLE = []
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
 
-    user_url = request.get('https://jsonplaceholder.typicode.com/users/' + id
-    name = user_url.get('name')
-    todos = user_url.get('https://jsonplaceholder.typicode.com/todos/').json
+user_url = request.get('https://jsonplaceholder.typicode.com/users/' + id)
+EMPLOYEE_NAME = user_url.get('name')
+todos = user_url.get('https://jsonplaceholder.typicode.com/todos/').json
 
-    for i in todos:
-        if i.get('completed') is True:
-            task_title.append(i['title'])
-            completed += 1
-        total += 1
+for i in todos:
+    if i.get('completed') is True:
+        TASK_TITLE.append(i['title'])
+        NUMBER_OF_DONE_TASKS += 1
+    TOTAL_NUMBER_OF_TASKS += 1
 
 
-    print('Employee {} is done with tasks({:d}/{:d}):' \
-            .format(name, completed, total
-    for j in task_title:
-        print("\t {}".format(j))
+print('Employee {} is done with tasks({:d}/{:d}):'.format(EMPLOYEE_NAME,
+      NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+for j in TASK_TITLE:
+    print("\t {}".format(j))
