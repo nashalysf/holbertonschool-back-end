2f87fc33c671e41bb19fd11a484f17d59d6366bc
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-21 15:34:13 -0400

    exports info in csv format

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,2 +1,22 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""
+RESTFul API for employee CSV format
+"""
+import requests
+import sys
+import csv
+
+
 if __name__ == "__main__":
+    """ Gets employee todo information """
+    id = sys.argv[1]
+
+    user_url = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + id).json()
+    USERNAME = user_url.get('username')
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + id + '/todos')
+    with open("{}.csv".format(id), 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for i in todos.json():
+            writer.writerow([id, USERNAME, i.get('completed'), i.get('title')])
