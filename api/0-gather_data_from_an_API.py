2acd4abdce86e9cc5227ebc49c6f237369b94eba
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-21 23:58:53 -0400

    fixing tasks number output

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,27 +7,28 @@ import sys
 
 
 if __name__ == "__main__":
+
     """ Gets employee todo information """
     id = sys.argv[1]
-    TASK_TITLE = []
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
+    task_title = []
+    number_of_done_tasks = 0
+    total_number_of_tasks = 0
 
-user_url = requests.get(
-    'https://jsonplaceholder.typicode.com/users/' + id).json()
-EMPLOYEE_NAME = user_url.get('name')
-todos = requests.get('https://jsonplaceholder.typicode.com/todos/').json()
+    user_url = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + id).json()
+    employee_name = user_url.get('name')
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/users/" + id + "/todos").json()
 
-for i in todos:
-    if i.get('userId') == int(id):
-        if i.get('completed') is True:
-            TASK_TITLE.append(i['title'])
-            NUMBER_OF_DONE_TASKS += 1
-        TOTAL_NUMBER_OF_TASKS += 1
+    for task in todos:
+        if task.get('userId') == int(id):
+            if task.get('completed') is True:
+                task_title.append(task['title'])
+                number_of_done_tasks += 1
+            total_number_of_tasks += 1
 
+    print('Employee {} is done with tasks({:d}/{:d}):'.format(employee_name,
+          number_of_done_tasks, total_number_of_tasks))
 
-print('Employee {} is done with tasks({:d}/{:d}):'.format(EMPLOYEE_NAME,
-      NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-
-for j in TASK_TITLE:
-    print("\t {}".format(j))
+    for j in task_title:
+        print("\t {}".format(j))
--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,2 +1,33 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""
+RESTFul API for employee
+"""
+import requests
+import sys
+import json
+
 if __name__ == "__main__":
+    """ Gets employee todo information """
+    id = sys.argv[1]
+    TASK_TITLE = []
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+
+user_url = requests.get(
+    'https://jsonplaceholder.typicode.com/users/' + id).json()
+EMPLOYEE_NAME = user_url.get('name')
+todos = requests.get('https://jsonplaceholder.typicode.com/todos/').json()
+
+for i in todos:
+    if i.get('userId') == int(id):
+        if i.get('completed') is True:
+            TASK_TITLE.append(i['title'])
+            NUMBER_OF_DONE_TASKS += 1
+        TOTAL_NUMBER_OF_TASKS += 1
+
+
+print('Employee {} is done with tasks({:d}/{:d}):'.format(EMPLOYEE_NAME,
+      NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+for j in TASK_TITLE:
+    print("\t {}".format(j))
