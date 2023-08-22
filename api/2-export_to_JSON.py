760c785a354b4dd9ff4667ad09c6b1769ebf67cc
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-22 00:05:08 -0400

    fixing typos

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -11,20 +11,19 @@ if __name__ == "__main__":
     id = sys.argv[1]
     tasks = []
 
+    user_url = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + id).json()
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' + id).json()
 
-user_url = requests.get(
-    'https://jsonplaceholder.typicode.com/users/' + id).json()
-todos = requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId=' + id).json()
+    for task in todos:
+        task_data = {
+            'task': task['title'],
+            'completed': task['completed'],
+            'username': user_url['username']
+        }
+        tasks.append(task_data)
+    data = {id: tasks}
 
-for task in todos:
-    task_data = {
-        'task': task['title'],
-        'completed': task['completed'],
-        'username': user_url['username']
-    }
-    tasks.append(task_data)
-data = {id: tasks}
-
-with open(id + '.json', 'w') as f:
-    json.dump(data, f)
+    with open(id + '.json', 'w') as f:
+        json.dump(data, f)
